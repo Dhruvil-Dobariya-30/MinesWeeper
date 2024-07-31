2bf3e44f6894eae0ca0cbe65472ce67778f78b00
Dhruvil Dobariya <dhruvil.devoteeinfotech@gmail.com>
2024-07-31 18:29:58 +0530

    third commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function displayMines(row, col) {
     div += "<br/>";
     for (let j = 0; j < col; j++) {
       minesArray[i][j] = value++;
-      div += `<div class="mines" id="${minesArray[i][j]}">&nbsp;</div>`;
+      div += `<div class="mines data" id="${minesArray[i][j]}" onclick="validateMine(this.id)">&nbsp;</div>`;
     }
   }
 
@@ -65,10 +65,11 @@ function generateNumber(totalMine, row, col) {
     let randomCol = Math.floor(Math.random() * col);
 
     if (minesArray[randomRow][randomCol] !== "&#128163;") {
-      document.getElementById(`${minesArray[randomRow][randomCol]}`).innerHTML =
-        "&#128163;";
+      document.getElementById(
+        `${minesArray[randomRow][randomCol]}`
+      ).innerHTML = ` <span class="data">&#128163;</span>`;
       minesArray[randomRow][randomCol] = "&#128163;";
-      // indexArray.push([randomRow, randomCol].join(""));
+      indexArray.push([randomRow, randomCol]);
       currentMine++;
       sideValues(row, col);
     }
@@ -77,64 +78,101 @@ function generateNumber(totalMine, row, col) {
 }
 
 function sideValues(r, c) {
+  let numbersArray = Array(r)
+    .fill()
+    .map(() => Array(c).fill(0));
+
   for (let i = 0; i < r; i++) {
     for (let j = 0; j < c; j++) {
-      let count = 0;
       if (minesArray[i][j] == "&#128163;") {
         if (i > 0 && j > 0 && minesArray[i - 1][j - 1] != "&#128163;") {
-          document.getElementById(`${minesArray[i - 1][j - 1]}`).innerHTML =
-            incrementNumber(i - 1, j - 1);
+          numbersArray[i - 1][j - 1]++;
         }
         if (i > 0 && minesArray[i - 1][j] != "&#128163;") {
-          document.getElementById(`${minesArray[i - 1][j]}`).innerHTML =
-            incrementNumber(i - 1, j);
+          numbersArray[i - 1][j]++;
         }
         if (
           i > 0 &&
           j < minesArray.length - 1 &&
           minesArray[i - 1][j + 1] != "&#128163;"
         ) {
-          document.getElementById(`${minesArray[i - 1][j + 1]}`).innerHTML =
-            incrementNumber(i - 1, j + 1);
+          numbersArray[i - 1][j + 1]++;
         }
         if (j > 0 && minesArray[i][j - 1] != "&#128163;") {
-          document.getElementById(`${minesArray[i][j - 1]}`).innerHTML =
-            incrementNumber(i, j - 1);
+          numbersArray[i][j - 1]++;
         }
         if (j < minesArray.length - 1 && minesArray[i][j + 1] != "&#128163;") {
-          document.getElementById(`${minesArray[i][j + 1]}`).innerHTML =
-            incrementNumber(i, j + 1);
+          numbersArray[i][j + 1]++;
         }
         if (
           i < minesArray.length - 1 &&
           j > 0 &&
           minesArray[i + 1][j - 1] != "&#128163;"
         ) {
-          document.getElementById(`${minesArray[i + 1][j - 1]}`).innerHTML =
-            incrementNumber(i + 1, j - 1);
+          numbersArray[i + 1][j - 1]++;
         }
         if (i < minesArray.length - 1 && minesArray[i + 1][j] != "&#128163;") {
-          document.getElementById(`${minesArray[i + 1][j]}`).innerHTML =
-            incrementNumber(i + 1, j);
+          numbersArray[i + 1][j]++;
         }
         if (
           i < minesArray.length - 1 &&
           j < minesArray.length - 1 &&
           minesArray[i + 1][j + 1] != "&#128163;"
         ) {
-          document.getElementById(`${minesArray[i + 1][j + 1]}`).innerHTML =
-            incrementNumber(i + 1, j + 1);
+          numbersArray[i + 1][j + 1]++;
         }
+      }
+    }
+  }
+
+  for (let i = 0; i < r; i++) {
+    for (let j = 0; j < c; j++) {
+      if (minesArray[i][j] != "&#128163;" && numbersArray[i][j] > 0) {
+        document.getElementById(
+          `${minesArray[i][j]}`
+        ).innerHTML = `${numbersArray[i][j]}`;
       }
     }
   }
 }
 
-function incrementNumber(i, j) {
-  let getDiv = document.getElementById(`${minesArray[i][j]}`);
-  if (getDiv.innerHTML == "&nbsp;") {
-    return 1;
+function validateMine(id) {
+  let visible = document.getElementById(id);
+  visible.style.fontSize = "24px";
+
+  if (visible.innerHTML == "&nbsp;") {
+    openSpaces(id);
   } else {
-    return getDiv.innerHTML++;
+    if (visible.children[0].innerHTML !== "&nbsp;") {
+      console.log("GAME OVER!!!");
+      alert("GAME OVER!!!");
+    }
+  }
+}
+
+function openSpaces(id) {
+  console.log(id);
+  console.log("Space Found");
+
+  let i;
+  let j;
+
+  let count = 0;
+  for (let x in minesArray) {
+    for (let y in minesArray) {
+      if (minesArray[x][y] == id) {
+        i = x;
+        j = y;
+      } else {
+        count++;
+      }
+    }
+  }
+  console.log(i, j);
+
+  if (minesArray[i][j + 1] == "&nbsp;") {
+    console.log("object");
+    document.getElementById(`${minesArray[i][j]}`).style.backgroundColor =
+      "white";
   }
 }
